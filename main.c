64b0f668c0b4d53eab5565183cbff3e441fc15fe
flyguy-ty <truperti17@gmail.com>
2023-04-04 18:48:05 -0500

    adding hello world

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hey, Tyler! I'm gonna count to 10! Watch:\n");
+    for (int i = 1; i < 11; i++){
+        printf("%d\n", i);
+    }
+    printf("See? That wasn't so hard!");
+    return 0;
+}
